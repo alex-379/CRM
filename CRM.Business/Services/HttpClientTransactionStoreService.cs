85d796c5497bd4ebf9758a395bba42a568a595ae
alex-379 <alexey_379@mail.ru>
2024-06-04 03:21:45 +1000

    fix path getBalance

--- CRM.Business/Services/HttpClientTransactionStoreService.cs
+++ CRM.Business/Services/HttpClientTransactionStoreService.cs
@@ -12,7 +12,7 @@ public class HttpClientTransactionStoreService : IHttpClientTransactionStoreServ
     
     public HttpClientTransactionStoreService()
     {
-        _httpClient.BaseAddress = new Uri(Routes.TransactionStoreApi);
+        //_httpClient.BaseAddress = new Uri(Routes.TransactionStoreApi);
         _httpClient.Timeout = new TimeSpan(NumericData.HttpClientTimeoutHour, NumericData.HttpClientTimeoutMin, NumericData.HttpClientTimeoutSec);
         _options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
     }
@@ -24,7 +24,7 @@ public class HttpClientTransactionStoreService : IHttpClientTransactionStoreServ
 
     private async Task GetBalance()
     {
-        var response = await _httpClient.GetAsync(Routes.Balance);
+        var response = await _httpClient.GetAsync("https://194.87.210.5:11000/api/accounts/3fa85f64-5717-4562-b3fc-2c963f66afa1/balance");
         response.EnsureSuccessStatusCode();
         var content = await response.Content.ReadAsStringAsync();
         var balance = JsonSerializer.Deserialize<AccountBalanceResponse>(content, _options);
