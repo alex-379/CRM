864b332a27447f793de1a318dc0ccb04d31c5a0e
alex-379 <alexey_379@mail.ru>
2024-07-04 23:44:53 +1000

    fix logs

--- CRM.API/Consumers/ConsumersLogs.cs
+++ CRM.API/Consumers/ConsumersLogs.cs
@@ -2,5 +2,5 @@ namespace CRM.API.Consumers;
 
 public class ConsumersLogs
 {
-    public const string SettingsConsumer = "Getting current configuration: {jsonMessage} from Rates Provider";
+    public const string SettingsConsumer = "Getting current configuration: {jsonMessage} from Configuration Manager";
 }
